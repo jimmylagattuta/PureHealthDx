ee411c76ebf0957c7f47402edcb0794b25cd54c8
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-03-16 22:17:40 -0700

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CaptainAlvaradosHandyman
+module LightSEODev
   class Application < Rails::Application
     config.load_defaults 7.0
 
