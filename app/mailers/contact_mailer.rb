80be4852dcef7dffd20bd3e5697d0b43a2ce2f6d
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-05-21 00:19:25 -0700

    testing emailer template two

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,29 @@
 class ContactMailer < ApplicationMailer
-    default from: 'jimmy.lagattuta@gmail.com'
-  
+  default from: 'jimmy.lagattuta@gmail.com'
+
   def full_submission_email(data)
     @data = data
-    mail(
-      to: 'jimmy.lagattuta@gmail.com',
-      subject: "New Full Appointment Submission"
-    )
+
+    # Attach signatures if present
+    attach_base64_image(data["patientConsentForMedicalServicesSignature"], 'patient_signature.png')
+    attach_base64_image(data["informedConsentForHghReplacementTherapyPatientSignature"], 'hgh_patient_signature.png')
+    attach_base64_image(data["informedConsentForHghReplacementTherapyWitnessSignature"], 'hgh_witness_signature.png')
+    attach_base64_image(data["informedConsentForTestosteroneReplacementTherapyPatientSignature"], 'trt_patient_signature.png')
+    attach_base64_image(data["informedConsentForTestosteroneReplacementTherapyWitnessSignature"], 'trt_witness_signature.png')
+    attach_base64_image(data["controlledSubstanceAutoRefillConsentPatientSignature"], 'controlled_signature.png')
+
+    mail(to: 'jimmy.lagattuta@gmail.com', subject: "New Full Appointment Submission")
+  end
+
+  private
+
+  def attach_base64_image(data_uri, filename)
+    return unless data_uri.present?
+
+    image_data = data_uri.sub(/^data:image\/png;base64,/, '')
+    attachments.inline[filename] = {
+      mime_type: 'image/png',
+      content: Base64.decode64(image_data)
+    }
   end
 end
