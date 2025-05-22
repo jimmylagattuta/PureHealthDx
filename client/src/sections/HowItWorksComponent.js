18ce1f41448440863a8f3f430789584e9c9fdf41
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-05-22 15:53:09 -0700

    rendering correct aspect ratios

--- client/src/sections/HowItWorksComponent.js
+++ client/src/sections/HowItWorksComponent.js
@@ -85,7 +85,7 @@ function HowItWorksComponent() {
         <div className="hiw-step">
           <div className="hiw-step-icon">
             <img
-              src="https://i.postimg.cc/V6Q0jL8b/take-your-prescription-icon-1.webp"        // ← swap in your “labs + paper” SVG
+              src="https://i.postimg.cc/qRGkFp9Y/take-your-prescription-icon-2.webp"        // ← swap in your “labs + paper” SVG
               alt="Complete Labs"
               width="55"
               height="65"
