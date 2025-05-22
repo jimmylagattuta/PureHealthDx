e0758227b178fe0fcf029d106cba92edcc28b3d6
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-05-21 23:40:58 -0700

    fixing root level index.html

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,5 +1,6 @@
+# app/controllers/fallback_controller.rb
 class FallbackController < ActionController::Base
-    def index
-      render file: 'public/index.html'
-    end
+  def index
+    render file: Rails.root.join('public', 'index.html'), layout: false
   end
+end
