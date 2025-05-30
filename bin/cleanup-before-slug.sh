3fbd4f8808e354727fd4830437ec67590837eef8
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-05-30 04:31:39 -0700

    Fix script permissions

--- bin/cleanup-before-slug.sh
+++ bin/cleanup-before-slug.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-echo "🔥 cleanup-before-slug.sh: aggressively stripping slug bloat"
+echo "🔥 cleanup-before-slug.sh: stripping slug bloat"
 
-rm -rf client/node_modules
-rm -rf client/build
 rm -rf node_modules
+rm -rf client
 rm -rf tmp/cache
+rm -rf log
+rm -rf test
+rm -rf spec
 rm -rf vendor/bundle
 rm -rf vendor/ruby*
 
