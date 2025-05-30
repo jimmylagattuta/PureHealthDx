8695dff47183d74607ad10d6c156e12daee4b227
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-05-30 05:31:43 -0700

    Aggressive slug cleanup: remove logs, tmp, vendor, test

--- scripts/heroku-cleanup.sh
+++ scripts/heroku-cleanup.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
 echo "🔥 cleanup-before-slug.sh: aggressively stripping slug bloat"
-# Remove leftover Node stuff
+
+# Remove Node modules and lock files
 [ -d client/node_modules ] && rm -rf client/node_modules && echo "🗑 Removed client/node_modules"
 [ -f package-lock.json ] && rm -f package-lock.json && echo "🗑 Removed package-lock.json"
+[ -f yarn.lock ] && rm -f yarn.lock && echo "🗑 Removed yarn.lock"
 
-# Kill Ruby + binary bloat
+# Remove Ruby/vendor bloat
 [ -d vendor/bundle ] && rm -rf vendor/bundle && echo "🗑 Removed vendor/bundle"
-[ -d vendor/ruby-3.1.2 ] && rm -rf vendor/ruby-3.1.2 && echo "🗑 Removed vendor/ruby-3.1.2"
-[ -f bin/node ] && rm -f bin/node && echo "🗑 Removed bin/node"
-[ -f bin/node.exe ] && rm -f bin/node.exe && echo "🗑 Removed bin/node.exe"
+[ -d vendor/ruby-* ] && rm -rf vendor/ruby-* && echo "🗑 Removed vendor/ruby-*"
+
+# Remove bin folder
 [ -d bin ] && rm -rf bin && echo "🗑 Removed bin"
+
+# Remove test and log assets
+[ -d test ] && rm -rf test && echo "🗑 Removed test"
+[ -d log ] && rm -rf log && echo "🗑 Removed log"
+
+# Clear compiled assets if applicable
+[ -d tmp/cache ] && rm -rf tmp/cache && echo "🗑 Removed tmp/cache"
 
 echo "🏁 cleanup-before-slug.sh complete"
