7e62947432ad52de0436b78b0ab358a2be8e70ab
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-06-05 15:15:19 -0700

    testing wicked pdf take two

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,14 +1,27 @@
 # app/controllers/appointments_controller.rb
 class AppointmentsController < ApplicationController
+  require 'base64'
+  require 'tempfile'
+
   def pdf
     @appointment = Appointment.find(params[:id])
 
-    respond_to do |format|
-      format.pdf do
-        render pdf: "appointment_#{@appointment.id}",
-               template: "appointments/pdf",
-               formats: [:html]
-      end
+    if @appointment.signature.present?
+      base64_data = @appointment.signature.split(',')[1]
+      decoded_image = Base64.decode64(base64_data)
+      
+      tmp_file = Tempfile.new(["signature_#{@appointment.id}", ".png"], binmode: true)
+      tmp_file.write(decoded_image)
+      tmp_file.rewind
+
+      @signature_path = "file://#{tmp_file.path}"
+    else
+      @signature_path = nil
     end
+
+    render pdf: "appointment_#{@appointment.id}",
+          template: "appointments/pdf",
+          layout: false
   end
+
 end
