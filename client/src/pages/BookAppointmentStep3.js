3ed30afe714655919a75036fd7031908e0e4c880
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-06-05 19:42:16 -0700

    small range test of download pdf button, demo mode forms activated take two

--- client/src/pages/BookAppointmentStep3.js
+++ client/src/pages/BookAppointmentStep3.js
@@ -9,7 +9,7 @@ const BookAppointmentStep3 = () => {
   const navigate = useNavigate();
 
   // Mobile-first defaults for demonstration
-  const demoMode = true;
+  const demoMode = false;
 
   // Refs for signature canvas
   const sigPad = useRef(null);
