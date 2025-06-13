7ac7b0ad8a51abe90fc8c8c0fca66975f863f3c5
James Lagattuta <jimmy.lagattuta@gmail.com>
2025-06-12 23:13:05 -0700

    adding google reviews, take four

--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import "./Testimonials.css";
 
 export default function Testimonials() {
-  const widgetRef = useRef();
+  const widgetRef = useRef(null);
   const [scriptLoaded, setScriptLoaded] = useState(false);
 
   useEffect(() => {
@@ -18,12 +18,13 @@ export default function Testimonials() {
           observer.disconnect();
         }
       },
-      {
-        rootMargin: "0px 0px 300px 0px",
-      }
+      { rootMargin: "0px 0px 300px 0px" }
     );
 
-    if (widgetRef.current) observer.observe(widgetRef.current);
+    if (widgetRef.current) {
+      observer.observe(widgetRef.current);
+    }
+
     return () => observer.disconnect();
   }, [scriptLoaded]);
 
@@ -40,7 +41,7 @@ export default function Testimonials() {
       <div
         className="trustindex-widget"
         ref={widgetRef}
-        style={{ marginTop: "60px" }}
+        style={{ marginTop: "60px", width: "100%" }}
       >
         <div className="ti-widget"></div>
       </div>
